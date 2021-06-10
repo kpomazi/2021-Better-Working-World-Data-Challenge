4872eae7d4cc56afd51e00cfc7d00e6f39cc4397
Pomázi Krisztián <krisztianpomazi@gmail.com>
2021-06-10 08:44:02 +0200

    Update install-cube.sh

--- install-cube.sh
+++ install-cube.sh
@@ -42,10 +42,6 @@ if ! [[ $LOCAL = "true" ]]; then
   try pip3 install --upgrade pip
   try pip3 install docker-compose
   
-  sudo add-apt-repository ppa:graphics-drivers/ppa
-  sudo apt update
-  sudo apt install nvidia-450
-  
   curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
   curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
